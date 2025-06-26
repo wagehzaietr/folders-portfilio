c63aa5cc81d974c5e7fce55d0730614c80d2afbb
wagehzaietr <bad1game1@hotmail>
2025-06-26 20:26:23 +0400

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- import BrowserRouter
+import './index.css';
+import App from './App.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    // Add other routes here
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter> {/* <-- wrap with Router */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
